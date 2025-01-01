f1c758fc28cc4cdd5c9dc7e51292344e70b3b036
kojamad28 <kojamad28@gmail.com>
2025-01-01 16:44:36 +0900

    divided files for development and production

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,7 +15,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "silk",
     "accounts",
 ]
 
@@ -27,7 +26,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "silk.middleware.SilkyMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
